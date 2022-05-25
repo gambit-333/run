2e8dfd6807be34547f40d6af77c11170655a4922
gambit-333 <gambit-333@mail.com>
2022-05-25 08:22:58 +0300

    traning

---
+++ src/js/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){  /* document - наша HTML структура */ /* Код будет запускаться, когда HTML структура будет готова(ready) - ready(function()  */
+    $('.carousel__inner').slick();   /* с помощью библиотеки JQuery мы обращаемся к какому-то классу */
+  });
